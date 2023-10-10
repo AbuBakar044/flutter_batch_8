448c8480df58d8962263a40ca4f0736ba32315e8
AbuBakar044 <bhattiabubakar@gmail.com>
2023-10-10 13:59:43 +0500

    Added Data in Home Screen
    Added Data in Home Screen

--- lib/add_friend_screen.dart
+++ lib/add_friend_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_batch_8/home_screen.dart';
 
 class AddFriendScreen extends StatefulWidget {
   const AddFriendScreen({super.key});
@@ -9,55 +10,78 @@ class AddFriendScreen extends StatefulWidget {
 }
 
 class _AddFriendScreenState extends State<AddFriendScreen> {
-
-
   @override
   Widget build(BuildContext context) {
-
-    print('...................................this is width ${MediaQuery.of(context).size.width}');
-    print('...................................this is height ${MediaQuery.of(context).size.height}');
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.yellow,
-        title: Text('Add Friend'),
+        title: const Text('Add Friend'),
         centerTitle: true,
       ),
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 30.0,
-              ),
-              const CircleAvatar(
-                radius: 60.0,
-                backgroundColor: Colors.black,
-              ),
-              const SizedBox(
-                height: 50.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Add Name',
-                  border: OutlineInputBorder(),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 30.0,
                 ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                maxLines: 10,
-                decoration: InputDecoration(
-                  hintText: 'Add Description',
-                  border: OutlineInputBorder(),
+                const CircleAvatar(
+                  radius: 60.0,
+                  backgroundColor: Colors.black,
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 50.0,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Add Name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'Mobile Number', border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  maxLines: 10,
+                  decoration: const InputDecoration(
+                    hintText: 'Add Description',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    //Navigator.pop(context);
+                  },
+                  child: const Text('Save Friend'),
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                      Colors.yellow,
+                    ),
+                    fixedSize: MaterialStatePropertyAll(
+                      Size(
+                        150,
+                        50,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
